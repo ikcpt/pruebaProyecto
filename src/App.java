addedb3058043a0f7995b5f42a2a4e2057ebd486
Pablo <ikdgq@plaiaundi.net>
2024-12-11 10:32:24 +0100

    commit mio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, new Branch1");
+        System.out.println("Hello, puto");
     }
 }
